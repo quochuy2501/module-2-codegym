4b2ac5e6fd0cb75b266656ec149d1c91fd1d819f
quochuy2501 <quochuy2001dn@gmail.com>
2024-01-30 01:28:36 +0700

    bai14

--- module2/src/bai14/InsertSort.java
+++ module2/src/bai14/InsertSort.java
@@ -1,15 +1,15 @@
 package bai14;
 
 public class InsertSort {
-    static int[] array = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    static int[] array = {2, 3, 0, 4, 7, 1, -9, 8, 44, 10};
 
     public static void insertSort(int[] array) {
         int pos, x;
-        for (int i = 1; i < array.length; i++) { //đoạn array[0] đã sắp xếp
+        for (int i = 1; i < array.length; i++) {
             x = array[i];
             pos = i;
             while (pos > 0 && x < array[pos - 1]) {
-                array[pos] = array[pos - 1]; // đổi chỗ
+                array[pos] = array[pos - 1];
                 pos--;
             }
             array[pos] = x;
